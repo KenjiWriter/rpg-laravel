ec2edf2550f5015970cae7f229c6729cd499efad
Maciej Wiśniewski <m.wisniewski@elpromaelectronics.com>
2021-09-27 16:13:56 +0200

    Added Livewire to the project

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -5,8 +5,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Demons of War - Online rpg game</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+	@livewireStyles
 </head>
 
 <body>
